4a72f79597d15a8ce1e7adc5763b2981b2eda508
cameldjoulako <cameldjoulako17@gmail.com>
2026-03-01 13:38:21 +0100

    flox control pratice and input and output signal practice

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 //import { Test } from './test/test';
 import { Employee } from './employee/employee';
 import { EmployeeI } from './models/employee.model';
+import { EmployeeList } from './employee-list/employee-list';
 /* import { DatePipe, CurrencyPipe, UpperCasePipe } from '@angular/common';
 import { LevelPipe } from './pipes/level-pipe'; */
 @Component({
   selector: 'app-root',
-  imports: [Employee /* LevelPipe */ /* DatePipe, CurrencyPipe, UpperCasePipe */],
+  imports: [Employee, EmployeeList /* LevelPipe */ /* DatePipe, CurrencyPipe, UpperCasePipe */],
   //template:
   templateUrl: './app.html',
   styleUrl: './app.scss',
@@ -18,20 +19,21 @@ export class App {
 
   //INPUT & OUTPUT
 
-  employee: EmployeeI = {
+  /* employee: EmployeeI = {
     _id: '69857452185789635874',
     name: 'Camel Djoulako',
     department: 'IT',
     level: 'J',
   };
+  $empl: any; */
 
-  onNameClick(employeeId: string) {
+  /* onNameClick(employeeId: string) {
     console.log(employeeId);
-  }
+  } */
 
   //FIN INPUT & OUTPUT
 
-  features: number = 5;
+  /* features: number = 5;
   protected readonly title = signal('manapp');
 
   myNeed: string = '';
@@ -64,9 +66,89 @@ export class App {
   toggleInputType() {
     console.log('Toggling input type');
     this.inputType = this.inputType === 'password' ? 'text' : 'password';
-  }
+  } */
 
   /* now: Date = new Date();
   amount: number = 12.89;
   appcurrency = 'FCFA '; */
+
+  employees: EmployeeI[] = [
+    {
+      _id: '69a335b1b786dd68e3f8a376',
+      name: 'Bartlett Bray',
+      department: 'IT',
+      level: 'M',
+    },
+    {
+      _id: '69a335b1706bf8b64856454f',
+      name: 'Brigitte Robbins',
+      department: 'Marketing',
+      level: 'S',
+    },
+    {
+      _id: '69a335b1d353f286f2df9a5e',
+      name: 'Amparo Watts',
+      department: 'IT',
+      level: 'M',
+    },
+    {
+      _id: '69a335b176db368eae1d629a',
+      name: 'Karen Wong',
+      department: 'Finance',
+      level: 'S',
+    },
+    {
+      _id: '69a335b14e6bacc62aea005e',
+      name: 'Gracie Robertson',
+      department: 'HR',
+      level: 'S',
+    },
+    {
+      _id: '69a335b18f3aaf1620d5c77e',
+      name: 'Natalie Haley',
+      department: 'HR',
+      level: 'M',
+    },
+    {
+      _id: '69a335b1a0b7fb7807eaa8e0',
+      name: 'Thelma George',
+      department: 'Marketing',
+      level: 'S',
+    },
+    {
+      _id: '69a335b103db0df7e5dc341e',
+      name: 'Ingrid Caldwell',
+      department: 'IT',
+      level: 'J',
+    },
+    {
+      _id: '69a335b1c86a15bcff273ca1',
+      name: 'Skinner Whitehead',
+      department: 'Marketing',
+      level: 'J',
+    },
+    {
+      _id: '69a335b1f3e287ce32256561',
+      name: 'Herminia Delacruz',
+      department: 'HR',
+      level: 'S',
+    },
+  ];
+
+  currentEmployee: null | EmployeeI = null;
+
+  showDetails(employeeId: string) {
+    //console.log(employeeId);
+    this.currentEmployee = this.employees.find((emp) => emp._id === employeeId) || null;
+  }
+
+  onDelete(employeeId: string) {
+    //console.log(employeeId);
+    this.currentEmployee = null;
+    this.employees = this.employees.filter((emp) => emp._id !== employeeId);
+  }
+
+  onEdit(employeeId: string) {
+    console.log(employeeId);
+  }
 }
---
+++ src/app/employee-list/employee-list.ts
@@ -0,0 +1,28 @@
+import { Component, input, output } from '@angular/core';
+import { EmployeeI } from '../models/employee.model';
+
+@Component({
+  selector: 'app-employee-list',
+  imports: [],
+  templateUrl: './employee-list.html',
+  styleUrl: './employee-list.scss',
+})
+export class EmployeeList {
+  employees = input.required<EmployeeI[]>();
+
+  edit = output<string>();
+  delete = output<string>();
+  details = output<string>();
+
+  onDetails(employeeId: string) {
+    this.details.emit(employeeId);
+  }
+
+  onDelete(employeeId: string) {
+    this.delete.emit(employeeId);
+  }
+
+  onEdit(employeeId: string) {
+    this.edit.emit(employeeId);
+  }
+}
